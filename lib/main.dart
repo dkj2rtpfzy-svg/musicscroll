7675000dfa6f7867f60426cb2d6bbbf8af4ccde7
David Da Cunha Silva <dkj2rtpfzy@privaterelay.appleid.com>
2026-07-16 07:28:23 +0200

    Sprint 017A - Initialisation de Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialisation de Hive
+  await Hive.initFlutter();
 
   runApp(
     const ProviderScope(
